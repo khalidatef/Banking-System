e7c82593fa5f7f0f4494aa38eeabad359b8ee7bd
Esraa_Ahmed <133446404+EsraaAhmed65@users.noreply.github.com>
2025-08-27 03:10:01 +0300

    feat(auth): add IUser and mock users

--- src/app/shared/data/mock-users.data.ts
+++ src/app/shared/data/mock-users.data.ts
@@ -1,2 +1,21 @@
-export class MockUsers {
-}
+import { IUser } from '../models/iuser.model';
+
+export const MOCK_USERS: IUser[] = [
+  {
+    id: 1,
+    userName: 'admin',
+    password: 'admin123',
+    role: 'Admin',
+    isActive: true,
+    email: 'admin@securebank.io',
+    phone: '01000000000'
+   {
+    id: 2,
+    userName: 'esraa',
+    password: 'esraa1',
+    role: 'User',
+    isActive: true,
+    email: 'esraa@mail.com',
+    phone: '01222222222'
+  }
+];
