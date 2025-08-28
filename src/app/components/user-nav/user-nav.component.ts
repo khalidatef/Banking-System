33b51f1aace472dfa13a23ae2a505104c5c3df4b
Esraa_Ahmed <133446404+EsraaAhmed65@users.noreply.github.com>
2025-08-28 19:18:47 +0300

    enable logout & display username in navbar

--- src/app/components/user-nav/user-nav.component.ts
+++ src/app/components/user-nav/user-nav.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-user-nav',
   standalone: true,
-  imports: [RouterLink,RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './user-nav.component.html',
   styleUrl: './user-nav.component.css'
 })
 export class UserNavComponent {
+  constructor(private auth: AuthService, private router: Router) { }
+
+  get username(): string | null { return this.auth.getUsername(); }
+  get role(): 'Admin' | 'User' | null { return this.auth.getRole(); }
+
+  logout() {
+    this.auth.logout();
+    this.router.navigate(['/auth/login']);
+  }
 
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -3,24 +3,30 @@ import { isPlatformBrowser } from '@angular/common';
 import { users, Role } from '../data/mock-users';
 
 const ROLE_KEY = 'role';
+const USERNAME_KEY = 'username';
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
   private platformId = inject(PLATFORM_ID);
   private isBrowser = isPlatformBrowser(this.platformId);
-  private roleCache: Role | null = null; 
-
+  private roleCache: Role | null = null;
+  private usernameCache: string | null = null;
   login(username: string, password: string): Role | null {
     const u = users.find(x => x.username === username && x.password === password && x.isActive);
     if (!u) return null;
     this.roleCache = u.role;
-    if (this.isBrowser) localStorage.setItem(ROLE_KEY, u.role);
+    this.usernameCache = u.username;
+    if (this.isBrowser)
+    localStorage.setItem(ROLE_KEY, u.role);
+    localStorage.setItem(USERNAME_KEY, u.username);
     return u.role;
   }
 
   logout(): void {
     this.roleCache = null;
-    if (this.isBrowser) localStorage.removeItem(ROLE_KEY);
+    if (this.isBrowser) 
+    localStorage.removeItem(ROLE_KEY);
+    localStorage.removeItem(USERNAME_KEY); 
   }
 
   getRole(): Role | null {
@@ -28,7 +34,12 @@ export class AuthService {
       const v = localStorage.getItem(ROLE_KEY);
       return v === 'Admin' || v === 'User' ? (v as Role) : null;
     }
-    return this.roleCache; 
+    return this.roleCache;
+  }
+
+    getUsername(): string | null {
+    if (this.isBrowser) return localStorage.getItem(USERNAME_KEY);
+    return this.usernameCache;
   }
 
   isLoggedIn(): boolean {
