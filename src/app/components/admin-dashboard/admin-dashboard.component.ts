1c7e0fd32d938e9e52de1af37e4ac99a5d192eaf
Omarateff1 <omar.atef3575@gmail.com>
2025-08-30 01:22:03 +0300

    Addedhomepages

--- src/app/components/admin-dashboard/admin-dashboard.component.ts
+++ src/app/components/admin-dashboard/admin-dashboard.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
-import { RouterLink } from "@angular/router";
+import { RouterLink } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-admin-dashboard',
   standalone: true,
   imports: [RouterLink],
   templateUrl: './admin-dashboard.component.html',
-  styleUrl: './admin-dashboard.component.css'
+  styleUrl: './admin-dashboard.component.css',
 })
 export class AdminDashboardComponent {
-
+  constructor(private _AuthService: AuthService) {}
+  get username(): string | null {
+    return this._AuthService.getUsername();
+  }
+  get role(): 'Admin' | 'User' | null {
+    return this._AuthService.getRole();
+  }
 }
--- src/app/components/user-dashboard/user-dashboard.component.ts
+++ src/app/components/user-dashboard/user-dashboard.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-user-dashboard',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './user-dashboard.component.html',
-  styleUrl: './user-dashboard.component.css'
+  styleUrl: './user-dashboard.component.css',
 })
 export class UserDashboardComponent {
-
+  constructor(private _AuthService: AuthService) {}
+  get username(): string | null {
+    return this._AuthService.getUsername();
+  }
+  get role(): 'Admin' | 'User' | null {
+    return this._AuthService.getRole();
+  }
 }
