aefefacbe8060a8d1c6f24053e6b4b45c36b97bb
arwaehab <136165947+arwaehab@users.noreply.github.com>
2025-08-29 04:59:16 +0300

    addedAdminHome

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,7 +10,6 @@ import { AdminDashboardComponent } from './components/admin-dashboard/admin-dash
 import { AdminPanelComponent } from './components/admin-panel/admin-panel.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'user', pathMatch: 'full' },
   {
     path: 'user',
     component: UserLayoutComponent,
